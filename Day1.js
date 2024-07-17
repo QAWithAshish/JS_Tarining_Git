e9689fa4260b1d453efda6296690c01311ac18ce
Ashish Bhalray <ashish.bhalray14@gmail.com>
2024-07-17 15:51:43 +0530

    Day 1 assignments

---
+++ Day1.js
@@ -0,0 +1,27 @@
+
+//Hello, World print 
+let grettings = "Hello, World!"
+
+console.log(grettings)
+
+
+//Basic operation with difrrent dataTypes
+let name = "Ashish"
+
+let age =25;
+
+let hight = 5.4;
+
+let value = true;
+
+let person = { name: 'Ashish', age: 30 };
+
+console.log(name,age,hight,value);
+console.log(person);
+
+//Concate two string 
+
+let str1= "Hello";
+let str2= "Ashish";
+
+console.log(str1+str2)
