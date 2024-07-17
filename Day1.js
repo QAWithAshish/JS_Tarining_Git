cb3d3d184e0e5e6a9a5fc3bba742754f5dc15bc6
Ashish Bhalray <ashish.bhalray14@gmail.com>
2024-07-17 17:09:23 +0530

    Changes done

--- Day1.js
+++ Day1.js
@@ -19,7 +19,7 @@ let person = { name: 'Ashish', age: 30 };
 console.log(name,age,hight,value);
 console.log(person);
 
-//Operation
+//Operators
 a=10,b=20;
 let sum= 10+20;
 let sub = b-a;
@@ -27,13 +27,23 @@ let mult= (a*b)/10
 let div = b/a;
 
 console.log("Sum of 10 and 20 is : ",sum);
-
 console.log("Substraction : ",sub);
 console.log("Multipliation : ",mult);
 console.log("Division : ",div);
 
-//Concate two string 
+//Comparison Operators:
+let isEqual = (10 == '10'); // ==  for comapraing same values || true
+let isStrictEqual = (10 === '10'); // === it will check the values and datatypes as well || false 
+let isNotEqual = (10 != 5); // true 
+let isGreaterThan = (10 > 5); // true
 
+//Logical Operators
+let andOperator = (true && false); // false || When both will values true its given true or else gives false
+let orOperator = (true || false); // true || If any one value is true it will give true 
+let notOperator = (!true); // false || Just change the oposite for given value 
+
+
+//Concate two string 
 let str1= "Hello";
 let str2= "Ashish";
 
